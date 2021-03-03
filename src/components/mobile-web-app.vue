3f685799bbdf31e3edbac74d0966035027d4d4fe
Coty Condry <ccondry@cisco.com>
2021-03-03 17:53:29 -0600

    add mobile web app QR code image, and update the text for the mobile web app

--- src/components/mobile-web-app.vue
+++ src/components/mobile-web-app.vue
@@ -1,26 +1,48 @@
 <template>
   <panel title="dCloud CC Mobile Web App Settings" aria-id="mobile-web-app">
     <p>
-      Use this information to start the dCloud CC mobile web app demo.
+      Use
+      <strong><a :href="url" target="_blank">this URL</a></strong>
+      <copy :value="url" name="Mobile Web App URL" />
+      or the following QR code to go to the dCloud CC Mobile Web App:
+      <center><vue-qr :text="url" :logoSrc="favicon" /></center>
     </p>
-    <ul>
-      <li>Datacenter: <strong>{{ datacenterDisplayName }}</strong></li>
-      <li>Session: <strong>{{ sessionId }}</strong></li>
-      <li>User ID: <strong>{{ jwtUser.id }}</strong></li>
-    </ul>
   </panel>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import { addUrlQueryParams } from '../utils'
+import VueQr from 'vue-qr'
+import favicon from '../assets/images/favicon.png'
+ 
 export default {
+  components: {
+    VueQr
+  },
+
+  data () {
+    return {
+      favicon
+    }
+  },
+
   computed: {
     ...mapGetters([
       'sessionId',
-      'datacenterDisplayName',
+      'datacenter',
       'jwtUser'
-    ])
+    ]),
+    url () {
+      const url = 'https://mc.cxdemo.net/'
+      const query = {
+        datacenter: this.datacenter,
+        sessionId: this.sessionId,
+        userId: this.jwtUser.id,
+        username: this.jwtUser.firstName + ' ' + this.jwtUser.lastName
+      }
+      return addUrlQueryParams(url, query)
+    }
   }
 }
 </script>
